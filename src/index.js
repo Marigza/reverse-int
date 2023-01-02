e4afa4fbd0ccc75b9cafc403edddd26c584078ee
Marigza <marigola@mail.ru>
2023-01-02 14:52:53 +0300

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let number = Math.abs(n);
+  let str = String(number);
+  let arr = str.split('');
+  let arrReverse = arr.reverse();
+  let result = arrReverse.join('');
+  return Number(result);
+
 }
